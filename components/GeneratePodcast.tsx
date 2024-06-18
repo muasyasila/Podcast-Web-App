894b1cf339dce488b1c73a077bb6df7398475c2c
muasyasila <curtissila4@gmail.com>
2024-06-18 22:30:39 +0300

    updates

---
+++ components/GeneratePodcast.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const GeneratePodcast = ({
+  setAudioStorageId,
+  setAudio,
+  voiceType,
+  audio,
+  voicePrompt,
+  setVoicePrompt,
+  setAudioDuration,
+}) => {
+  return <div>GeneratePodcast</div>;
+};
+
+export default GeneratePodcast;
