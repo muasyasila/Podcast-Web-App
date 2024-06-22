7e8f0e86216dff8e14504dfd55c48ad80b797819
muasyasila <curtissila4@gmail.com>
2024-06-22 11:01:26 +0300

    audio generation

--- convex/openai.ts
+++ convex/openai.ts
@@ -2,6 +2,7 @@ import { action } from "./_generated/server";
 import { v } from "convex/values";
 
 import OpenAI from "openai";
+import { SpeechCreateParams } from "openai/resources/audio/speech.mjs";
 
 const openai = new OpenAI({
    apiKey: process.env.OPENAI_API_KEY 
@@ -9,16 +10,15 @@ const openai = new OpenAI({
 
 export const generateAudioAction = action({
   args: { input: v.string(), voice: v.string() },
-  handler: async (_, args) => {
+  handler: async (_, { voice, input }) => {
     const mp3 = await openai.audio.speech.create({
         model: "tts-1",
-        voice: "alloy",
-        input: "Today is a wonderful day to build something people love!",
+        voice: voice as SpeechCreateParams['voice'],
+        input,
       });
-      console.log(speechFile);
-      const buffer = Buffer.from(await mp3.arrayBuffer());
-      await fs.promises.writeFile(speechFile, buffer);
+      
+      const buffer = await mp3.arrayBuffer();
 
-    return "success";
+    return buffer;
   },
 });
